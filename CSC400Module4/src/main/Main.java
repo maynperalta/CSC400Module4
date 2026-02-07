80f16a895f2a6314680d64ea86a7af240856b957
Maynard Peralta <maynperalta@gmail.com>
2026-02-06 22:49:56 -0700

    Added switch case for operator elements.

--- CSC400Module4/src/main/Main.java
+++ CSC400Module4/src/main/Main.java
@@ -1,6 +1,63 @@
 package main;
 
+import java.util.Stack;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
+	
+	public int postfixCalc(String postfixEq) {
+		Stack<Integer> stack = new Stack<>();
+		String[] elements = postfixEq.trim().split("\\s+");
+			
+		for (String element : elements) {
+			if(isNumeric(element)) {
+				stack.push(Integer.parseInt(element));
+			} else if (isOperator(element)) {
+				if (stack.size() < 2) {
+					System.out.println("Error: Invalid postfix expression");
+					return Integer.MIN_VALUE;
+				}
+				int b = stack.pop();
+				int a = stack.pop();
+				int result = 0;
+				
+				switch (element) {
+				case "+":
+					result = a + b;
+					break;
+				case "-":
+					result = a - b;
+					break;
+				case "*":
+					result = a * b;
+					break;
+				case "/":
+					if (b == 0) {
+						System.out.println("Error: Cannot divide by zero.");
+						return Integer.MIN_VALUE;
+					}
+					result = a / b;
+					break;
+				case "%":
+					if (b == 0) {
+						System.out.println("Error: Cannot divide by zero.");
+						return Integer.MIN_VALUE;
+					}
+					result = a % b;
+					break;
+				default:
+					System.out.println("Error: Invalid operator " + element);
+					return Integer.MIN_VALUE;						
+				}
+				stack.push(result);
+			} else {
+				System.out.println("Error: Invalid element: " + element);
+				return Integer.MIN_VALUE;
+			}
+		}
+	}
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
